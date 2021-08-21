5f694dfce4d1cf897f13e31db81c9edfd0fdc2e0
Fernando Vasquez <53883345+Swumplurd@users.noreply.github.com>
2021-08-20 20:05:14 -0500

    la ultima tarea agregada es la primera en mostrarse

--- src/components/TodoApp/todoReducer.js
+++ src/components/TodoApp/todoReducer.js
@@ -1,7 +1,7 @@
 export const todoReducer = (state = [], action) => {
     switch (action.type) {
         case "add":
-            return [...state, action.payload];
+            return [action.payload, ...state];
         case "delete":
             return state.filter((todo) => todo.id !== action.payload);
         /* case 'toggle':
